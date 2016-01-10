c408eafb16ac33c4241d6f4a22e3b1378ca01a7f
yeah <gitfu@users.noreply.github.com>
2016-01-10 16:47:29 -0500

    Update scc.js

--- scc.js
+++ scc.js
@@ -6,17 +6,19 @@ function parseSCC(input) {
     var cues = [];
     var re = /^(\d\d):(\d\d):(\d\d)([:;])(\d\d)\t(.*)/gm;
     var match;
+
     while ((match = re.exec(input)) !== null) {
         var time = 3600 * match[1] + 60 * match[2] + 1 * match[3];
         time += match[5] / 30; // FIXME: dropframe vs. non-dropframe
-        var bytes = [];
+	var stamp=String(match[1]+':'+match[2]+':'+match[3]+':'+match[5])
+	var bytes = [];
         match[6].split(/\s+/).forEach(function(pair) {
             if (/^[0-9a-f]{4}$/i.test(pair)) {
                 bytes.push(parseInt(pair.substring(0, 2), 16));
                 bytes.push(parseInt(pair.substring(2, 4), 16));
             }
         });
-        cues.push({ time: time, data: new Uint8Array(bytes) });
+        cues.push({ time: time,stamp:stamp, data: new Uint8Array(bytes) });
     }
     return cues;
 }
